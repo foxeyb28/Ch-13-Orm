9260e62f195dd67751112b3e017108db5cd69160
foxeyb28 <131126615+foxeyb28@users.noreply.github.com>
2023-09-21 15:12:13 -0500

    fixed the delete category in routes js

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const { json } = require('sequelize');
 const { Category, Product } = require('../../models');
 // do all of these 9/9/23
 // The `/api/categories` endpoint
@@ -36,43 +37,46 @@ try {
 router.post('/', async (req, res) => {
   // create a new category
   try {
-    const socksData = await catergoryData.create({
-      product_id: Product.body.product_id,
-    });
+    const socksData = await catergoryData.create(req.body);
     res.status(200).json(catergoryData);
-  } catch (err) {
+      
+    }
+    catch (err) { 
     res.status(400),json(err);
   }
 });
 
 router.put('/:catergory_id', async (req, res) => {
   // update a category by its `id` value
-  const updatedcatergoryData = await catergoryData.update(
-   { product: req.body.product,
-    category: req.body.category,
-    tag: req.body.tag,
-    productTag: req.body.productTag,
-   },
-  );
-});
-
-router.delete('/:id', async (req, res) => {
-  // delete a category by its `id` value
   try {
-    const Category = await catergoryData.destroy({
-      where: {
-        id: req.params.id,
-      },
-    });
-    if (!categoryDataData) {
-      res.status(404).json({ message: 'No category found with that id!' });
-      return;
-    }
-
-    res.status(200).json(libraryCardData);
-  } catch (err) {
-    
+    const catergoryData = await catergoryData.update(req.body, {
+where: {
+  id: req.params.id,
+},
+  });
+  if (!catergoryData[0]) {
+    res.status(404).json({ message: 'No category with this id!'});
+    return
   }
+  res.status(200),json(catergoryData);
+} catch (err) {
+  res.status(500).json(err);
+}
 });
+
+
+router.delete('/:category_id', (req, res) => {
+  // delete a category by its `id` value
+  
+     Category.destroy({
+      where: {
+        category_id: req.params.id,
+      },
+    })
+    .then((deletedCategory) => {
+      res.json(deletedCategory);
+    })
+    .catch((err) => res.json(err));
+  });
 
 module.exports = router;
